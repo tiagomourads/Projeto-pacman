64230cbb449522f639e81ce6347c2861ef31d20e
Tiago Moura <129202390+tiagomourads@users.noreply.github.com>
2023-06-07 18:39:21 -0300

    Update Pac-man.py

--- Principal.py
+++ Principal.py
@@ -2,7 +2,7 @@ import os
 from random import randint
 from time import sleep
 
-# Aqui inserimos as dimenções do layout.
+# Aqui inserimos as dimenções do Grid.
 
 largura = 10 
 altura = 10
@@ -15,11 +15,11 @@ comida = 'X'
 # Posição inicial do Pac-man
 posicao_pacman = [randint(0, largura - 1), randint(0, altura - 1)]
 
-# Posição inicial da comida no layout
+# Posição inicial da comida no Grid
 
 posicao_comida = [randint(0, largura - 1), randint(0, altura - 1)]
 
-# Comando onde a comida vai aparecer aleatoriamente no layout 
+# Definimos essa função para a comida reposicionar aleatoriamente no Grid
 def reposicionar_comida():
     posicao_comida[0] = randint(0, largura - 1)
     posicao_comida[1] = randint(0, altura - 1)
@@ -27,27 +27,27 @@ def reposicionar_comida():
 # Definimos essa função para a movimentação do pac-man em direção a comida
 
 def mover_pacman():
-    x_pacman, y_pacman = posicao_pacman
-    x_comida, y_comida = posicao_comida
+    moverX_pacman, moverY_pacman = posicao_pacman
+    moverX_comida, moverY_comida = posicao_comida
     sleep(0.3)
-    if x_pacman < x_comida:
-        x_pacman += 1
-    elif x_pacman > x_comida:
-        x_pacman -= 1
-    elif y_pacman < y_comida:
-        y_pacman += 1
-    elif y_pacman > y_comida:
-        y_pacman -= 1
+    if moverX_pacman < moverX_comida:
+        moverX_pacman += 1
+    elif moverX_pacman > moverX_comida:
+        moverX_pacman -= 1
+    elif moverY_pacman < moverY_comida:
+        moverY_pacman += 1
+    elif moverY_pacman > moverY_comida:
+        moverY_pacman -= 1
     
-    # Essas linhas de código servem para limitadas como coordenadas do Pac-Man dentro dos limites do layout
+    # Essas linhas de código serve para limitar, como coordenadas do Pac-Man dentro dos limites do Grid
     
-    posicao_pacman[0] = max(0, min(x_pacman, largura - 1))
-    posicao_pacman[1] = max(0, min(y_pacman, altura - 1))
+    posicao_pacman[0] = max(0, min(moverX_pacman, largura - 1))
+    posicao_pacman[1] = max(0, min(moverY_pacman, altura - 1))
 
-# Definimos mais uma função para a exibição do layout
+# Definimos mais uma função para a exibição do Grid
 
 def exibir_layout():
-    os.system('cls') # Foi importado o (os) para limpar o terminal e só exibir um layout com o pac-man se movimentando
+    os.system('cls') # Foi importado o (os) para limpar o terminal e só exibir um Grid com o pac-man se movimentando
     for coluna in range(altura):
         for linha in range(largura):
             if [linha, coluna] == posicao_pacman:
@@ -57,10 +57,10 @@ def exibir_layout():
              # Gerar a comida no local correto    
                 print(comida, end=' ')
             else:
-            # Caso contrario exibir o layout com varios pontos (.)
+            # Caso contrario exibir o Grid com varios pontos (.)
                 print('.', end=' ')
         print()
-# Esse é o loop principal do jogo,significa que o jogo continuar sendo executado até que uma condição seja atendida.
+# Esse é o loop principal do jogo,significa que o jogo vai continuar sendo executado até que uma condição seja atendida.
 while True:
     exibir_layout()
     mover_pacman()
